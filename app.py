8005cb32cd70169911de14d61f337f6781079323
Vagrant User <vagrant@multicloud>
2021-07-29 20:41:39 -0300

    Recap

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")                                                                                                                                   
 def pagina_inicial():                                                                                                                             
-    return "Laboratorio Pipeline DevOps"                                                                                                          
-    break
+    return "Hello World"                                                                                                          
 
 if __name__ == '__main__':
     app.run(debug=True)
